ba1eac2d4294db9f3b02178a2120ca1b8f78f1fb
BreaBrain <46314237+BreaBrain@users.noreply.github.com>
2019-08-01 11:30:36 +0200

    Update decrypt.sh

--- crypt/decrypt.sh
+++ crypt/decrypt.sh
@@ -6,40 +6,12 @@ t=$1
 #apt-get install ccrypt
 clear
 
-if [ ! -d /bin/encrypt ]
-then
-cp encrypt.sh /bin/encrypt
-cp decrypt.sh /bin/decrypt
-chmod +x /bin/encrypt
-chmod +x /bin/decrypt
-fi
-
 echo "Write a number between 1 and 10"
 read a
 echo "Another number between 1 and 10"
 read b
 echo "And an another number between 1 and 10"
 read c
-
-
-if [ ! -d /etc/encrypt/ ]
-then
-mkdir /etc/encrypt/
-mkdir /etc/encrypt/dontremove/
-if [ ! -f /etc/encrypt/dontremove/key0.txt ]
-then
-strings /dev/urandom |tr -dc A-Za-z0-9 | head -c5000; echo > /etc/encrypt/dontremove/key0.txt
-strings /dev/urandom |tr -dc A-Za-z0-9 | head -c5000; echo > /etc/encrypt/dontremove/key1.txt
-strings /dev/urandom |tr -dc A-Za-z0-9 | head -c5000; echo > /etc/encrypt/dontremove/key2.txt
-strings /dev/urandom |tr -dc A-Za-z0-9 | head -c5000; echo > /etc/encrypt/dontremove/key3.txt
-strings /dev/urandom |tr -dc A-Za-z0-9 | head -c5000; echo > /etc/encrypt/dontremove/key4.txt
-strings /dev/urandom |tr -dc A-Za-z0-9 | head -c5000; echo > /etc/encrypt/dontremove/key5.txt
-strings /dev/urandom |tr -dc A-Za-z0-9 | head -c5000; echo > /etc/encrypt/dontremove/key6.txt
-strings /dev/urandom |tr -dc A-Za-z0-9 | head -c5000; echo > /etc/encrypt/dontremove/key7.txt
-strings /dev/urandom |tr -dc A-Za-z0-9 | head -c5000; echo > /etc/encrypt/dontremove/key8.txt
-strings /dev/urandom |tr -dc A-Za-z0-9 | head -c5000; echo > /etc/encrypt/dontremove/key9.txt
-fi
-fi
 
 clear
 if [[ $t == "" ]]
@@ -150,6 +122,11 @@ then
 ccrypt -dr $t -k /etc/encrypt/dontremove/key8.txt > /dev/null || echo "Wrong Key"
 else
 ccrypt -dr $t -k /etc/encrypt/dontremove/key9.txt > /dev/null || echo "Wrong Key"
+fi
+
+if [ ! -d $t ]
+then
+t=$(echo $t | rev | cut -c 5- | rev )
 fi
 
 ls -l $t
