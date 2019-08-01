1cef9f7967906d871b5bd6bb200fcbbeaffb6cba
BreaBrain <46314237+BreaBrain@users.noreply.github.com>
2019-08-01 11:58:36 +0200

    Update encrypt.sh

--- crypt/encrypt.sh
+++ crypt/encrypt.sh
@@ -2,6 +2,8 @@
 
 t=$1
 
+clear
+
 echo "Write a number between 1 and 10"
 read a
 echo "Another number between 1 and 10"
@@ -9,7 +11,6 @@ read b
 echo "And an another number between 1 and 10"
 read c
 
-clear
 if [[ $t == "" ]]
 then
 ls -l
@@ -19,114 +20,114 @@ fi
 
 ccrypt -dr /etc/encrypt/dontremove -k /etc/encrypt/key/key.txt > /dev/null
 
-if [[ $a == 1 ]]
+if [[ $c == 1 ]]
 then
-ccrypt -er $t -k /etc/encrypt/dontremove/key0.txt > /dev/null || echo "Wrong Key"
-elif [[ $a == 2 ]]
+ccrypt -dr $t -k /etc/encrypt/dontremove/key0.txt > /dev/null || echo "Wrong Key"
+elif [[ $c == 2 ]]
 then
-ccrypt -er $t -k /etc/encrypt/dontremove/key1.txt > /dev/null || echo "Wrong Key"
-elif [[ $a == 3 ]]
+ccrypt -dr $t -k /etc/encrypt/dontremove/key1.txt > /dev/null || echo "Wrong Key"
+elif [[ $c == 3 ]]
 then
-ccrypt -er $t -k /etc/encrypt/dontremove/key2.txt > /dev/null || echo "Wrong Key"
-elif [[ $a == 4 ]]
+ccrypt -dr $t -k /etc/encrypt/dontremove/key2.txt > /dev/null || echo "Wrong Key"
+elif [[ $c == 4 ]]
 then
-ccrypt -er $t -k /etc/encrypt/dontremove/key3.txt > /dev/null || echo "Wrong Key"
-elif [[ $a == 5 ]]
+ccrypt -dr $t -k /etc/encrypt/dontremove/key3.txt > /dev/null || echo "Wrong Key"
+elif [[ $c == 5 ]]
 then
-ccrypt -er $t -k /etc/encrypt/dontremove/key4.txt > /dev/null || echo "Wrong Key"
-elif [[ $a == 6 ]]
+ccrypt -dr $t -k /etc/encrypt/dontremove/key4.txt > /dev/null || echo "Wrong Key"
+elif [[ $c == 6 ]]
 then
-ccrypt -er $t -k /etc/encrypt/dontremove/key5.txt > /dev/null || echo "Wrong Key"
-elif [[ $a == 7 ]]
+ccrypt -dr $t -k /etc/encrypt/dontremove/key5.txt > /dev/null || echo "Wrong Key"
+elif [[ $c == 7 ]]
 then
-ccrypt -er $t -k /etc/encrypt/dontremove/key6.txt > /dev/null || echo "Wrong Key"
-elif [[ $a == 8 ]]
+ccrypt -dr $t -k /etc/encrypt/dontremove/key6.txt > /dev/null || echo "Wrong Key"
+elif [[ $c == 8 ]]
 then
-ccrypt -er $t -k /etc/encrypt/dontremove/key7.txt > /dev/null || echo "Wrong Key"
-elif [[ $a == 9 ]]
+ccrypt -dr $t -k /etc/encrypt/dontremove/key7.txt > /dev/null || echo "Wrong Key"
+elif [[ $c == 9 ]]
 then
-ccrypt -er $t -k /etc/encrypt/dontremove/key8.txt > /dev/null || echo "Wrong Key"
+ccrypt -dr $t -k /etc/encrypt/dontremove/key8.txt > /dev/null || echo "Wrong Key"
 else
-ccrypt -er $t -k /etc/encrypt/dontremove/key9.txt > /dev/null || echo "Wrong Key"
+ccrypt -dr $t -k /etc/encrypt/dontremove/key9.txt > /dev/null || echo "Wrong Key"
 fi
 
 if [ ! -d $t ]
 then
-t="$t.cpt"
+t=$(echo $t | rev | cut -c 5- | rev )
 fi
 
 if [[ $b == 1 ]]
 then
-ccrypt -er $t -k /etc/encrypt/dontremove/key0.txt > /dev/null || echo "Wrong Key"
+ccrypt -dr $t -k /etc/encrypt/dontremove/key0.txt > /dev/null || echo "Wrong Key"
 elif [[ $b == 2 ]]
 then
-ccrypt -er $t -k /etc/encrypt/dontremove/key1.txt > /dev/null || echo "Wrong Key"
+ccrypt -dr $t -k /etc/encrypt/dontremove/key1.txt > /dev/null || echo "Wrong Key"
 elif [[ $b == 3 ]]
 then
-ccrypt -er $t -k /etc/encrypt/dontremove/key2.txt > /dev/null || echo "Wrong Key"
+ccrypt -dr $t -k /etc/encrypt/dontremove/key2.txt > /dev/null || echo "Wrong Key"
 elif [[ $b == 4 ]]
 then
-ccrypt -er $t -k /etc/encrypt/dontremove/key3.txt > /dev/null || echo "Wrong Key"
+ccrypt -dr $t -k /etc/encrypt/dontremove/key3.txt > /dev/null || echo "Wrong Key"
 elif [[ $b == 5 ]]
 then
-ccrypt -er $t -k /etc/encrypt/dontremove/key4.txt > /dev/null || echo "Wrong Key"
+ccrypt -dr $t -k /etc/encrypt/dontremove/key4.txt > /dev/null || echo "Wrong Key"
 elif [[ $b == 6 ]]
 then
-ccrypt -er $t -k /etc/encrypt/dontremove/key5.txt > /dev/null || echo "Wrong Key"
+ccrypt -dr $t -k /etc/encrypt/dontremove/key5.txt > /dev/null || echo "Wrong Key"
 elif [[ $b == 7 ]]
 then
-ccrypt -er $t -k /etc/encrypt/dontremove/key6.txt > /dev/null || echo "Wrong Key"
+ccrypt -dr $t -k /etc/encrypt/dontremove/key6.txt > /dev/null || echo "Wrong Key"
 elif [[ $b == 8 ]]
 then
-ccrypt -er $t -k /etc/encrypt/dontremove/key7.txt > /dev/null || echo "Wrong Key"
+ccrypt -dr $t -k /etc/encrypt/dontremove/key7.txt > /dev/null || echo "Wrong Key"
 elif [[ $b == 9 ]]
 then
-ccrypt -er $t -k /etc/encrypt/dontremove/key8.txt > /dev/null || echo "Wrong Key"
+ccrypt -dr $t -k /etc/encrypt/dontremove/key8.txt > /dev/null || echo "Wrong Key"
 else
-ccrypt -er $t -k /etc/encrypt/dontremove/key9.txt > /dev/null || echo "Wrong Key"
+ccrypt -dr $t -k /etc/encrypt/dontremove/key9.txt > /dev/null || echo "Wrong Key"
 fi
 
 if [ ! -d $t ]
 then
-t="$t.cpt"
+t=$(echo $t | rev | cut -c 5- | rev )
 fi
 
-if [[ $c == 1 ]]
+if [[ $a == 1 ]]
 then
-ccrypt -er $t -k /etc/encrypt/dontremove/key0.txt > /dev/null || echo "Wrong Key"
-elif [[ $c == 2 ]]
+ccrypt -dr $t -k /etc/encrypt/dontremove/key0.txt > /dev/null || echo "Wrong Key"
+elif [[ $a == 2 ]]
 then
-ccrypt -er $t -k /etc/encrypt/dontremove/key1.txt > /dev/null || echo "Wrong Key"
-elif [[ $c == 3 ]]
+ccrypt -dr $t -k /etc/encrypt/dontremove/key1.txt > /dev/null || echo "Wrong Key"
+elif [[ $a == 3 ]]
 then
-ccrypt -er $t -k /etc/encrypt/dontremove/key2.txt > /dev/null || echo "Wrong Key"
-elif [[ $c == 4 ]]
+ccrypt -dr $t -k /etc/encrypt/dontremove/key2.txt > /dev/null || echo "Wrong Key"
+elif [[ $a == 4 ]]
 then
-ccrypt -er $t -k /etc/encrypt/dontremove/key3.txt > /dev/null || echo "Wrong Key"
-elif [[ $c == 5 ]]
+ccrypt -dr $t -k /etc/encrypt/dontremove/key3.txt > /dev/null || echo "Wrong Key"
+elif [[ $a == 5 ]]
 then
-ccrypt -er $t -k /etc/encrypt/dontremove/key4.txt > /dev/null || echo "Wrong Key"
-elif [[ $c == 6 ]]
+ccrypt -dr $t -k /etc/encrypt/dontremove/key4.txt > /dev/null || echo "Wrong Key"
+elif [[ $a == 6 ]]
 then
-ccrypt -er $t -k /etc/encrypt/dontremove/key5.txt > /dev/null || echo "Wrong Key"
-elif [[ $c == 7 ]]
+ccrypt -dr $t -k /etc/encrypt/dontremove/key5.txt > /dev/null || echo "Wrong Key"
+elif [[ $a == 7 ]]
 then
-ccrypt -er $t -k /etc/encrypt/dontremove/key6.txt > /dev/null || echo "Wrong Key"
-elif [[ $c == 8 ]]
+ccrypt -dr $t -k /etc/encrypt/dontremove/key6.txt > /dev/null || echo "Wrong Key"
+elif [[ $a == 8 ]]
 then
-ccrypt -er $t -k /etc/encrypt/dontremove/key7.txt > /dev/null || echo "Wrong Key"
-elif [[ $c == 9 ]]
+ccrypt -dr $t -k /etc/encrypt/dontremove/key7.txt > /dev/null || echo "Wrong Key"
+elif [[ $a == 9 ]]
 then
-ccrypt -er $t -k /etc/encrypt/dontremove/key8.txt > /dev/null || echo "Wrong Key"
+ccrypt -dr $t -k /etc/encrypt/dontremove/key8.txt > /dev/null || echo "Wrong Key"
 else
-ccrypt -er $t -k /etc/encrypt/dontremove/key9.txt > /dev/null || echo "Wrong Key"
+ccrypt -dr $t -k /etc/encrypt/dontremove/key9.txt > /dev/null || echo "Wrong Key"
 fi
 
 ccrypt -er /etc/encrypt/dontremove -k /etc/encrypt/key/key.txt > /dev/null
 
 if [ ! -d $t ]
 then
-t="$t.cpt"
+t=$(echo $t | rev | cut -c 5- | rev )
 fi
 
 ls -l $t
